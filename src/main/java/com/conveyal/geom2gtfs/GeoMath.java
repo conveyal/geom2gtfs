de4f168f39f4a44865853772d7c7921455638441
Brandon Martin-Anderson <badhill@gmail.com>
2014-04-24 14:28:20 -0700

    moved a things into the helper class 'GeoMath'

---
+++ src/main/java/com/conveyal/geom2gtfs/GeoMath.java
@@ -0,0 +1,41 @@
+package com.conveyal.geom2gtfs;
+
+import com.vividsolutions.jts.geom.Coordinate;
+
+public class GeoMath {
+	static double greatCircle(double x1deg, double y1deg, double x2deg, double y2deg){
+        double x1 = Math.toRadians(x1deg);
+        double y1 = Math.toRadians(y1deg);
+        double x2 = Math.toRadians(x2deg);
+        double y2 = Math.toRadians(y2deg);
+		
+		// haverside formula
+	    double a = Math.pow(Math.sin((x2-x1)/2), 2)
+	             + Math.cos(x1) * Math.cos(x2) * Math.pow(Math.sin((y2-y1)/2), 2);
+	
+	    // great circle distance in radians
+	    double angle2 = 2 * Math.asin(Math.min(1, Math.sqrt(a)));
+	
+	    // convert back to degrees
+	    angle2 = Math.toDegrees(angle2);
+	
+	    // each degree on a great circle of Earth is 60 nautical miles
+	    double distNautMiles = 60 * angle2;
+	    
+	    double distMeters = distNautMiles * 1852;
+	    return distMeters;
+	}
+	
+	static double greatCircle( Coordinate p1, Coordinate p2 ){
+		return greatCircle( p1.x, p1.y, p2.x, p2.y );
+	}
+	
+	static Coordinate interpolate(Coordinate p1, Coordinate p2,
+			double index) {
+		
+		double x = (p2.x-p1.x)*index + p1.x;
+		double y = (p2.y-p1.y)*index + p1.y;
+		
+		return new Coordinate(x,y);
+	}
+}
--- src/main/java/com/conveyal/geom2gtfs/Main.java
+++ src/main/java/com/conveyal/geom2gtfs/Main.java
@@ -205,12 +205,12 @@ public class Main {
 			Coordinate p2 = coords[i+1];
 			
 			double segCurs = overshot;
-			double segLen = greatCircle( p1, p2 );
+			double segLen = GeoMath.greatCircle( p1, p2 );
 			totalLen+=segLen;
 			
 			while( segCurs < segLen ){
 				double index = segCurs/segLen;
-				Coordinate interp = interpolate( p1, p2, index );
+				Coordinate interp = GeoMath.interpolate( p1, p2, index );
 				
 				ProtoRouteStop prs = new ProtoRouteStop();
 				prs.coord = interp;
@@ -236,14 +236,7 @@ public class Main {
 		return ret;
 	}
 
-	private static Coordinate interpolate(Coordinate p1, Coordinate p2,
-			double index) {
-		
-		double x = (p2.x-p1.x)*index + p1.x;
-		double y = (p2.y-p1.y)*index + p1.y;
-		
-		return new Coordinate(x,y);
-	}
+
 
 	private static FeatureSource<?, ?> getFeatureSource(String shp_filename)
 			throws MalformedURLException, IOException {
@@ -259,31 +252,6 @@ public class Main {
 		return source;
 	}
 	
-	static double greatCircle(double x1deg, double y1deg, double x2deg, double y2deg){
-        double x1 = Math.toRadians(x1deg);
-        double y1 = Math.toRadians(y1deg);
-        double x2 = Math.toRadians(x2deg);
-        double y2 = Math.toRadians(y2deg);
-		
-		// haverside formula
-	    double a = Math.pow(Math.sin((x2-x1)/2), 2)
-	             + Math.cos(x1) * Math.cos(x2) * Math.pow(Math.sin((y2-y1)/2), 2);
-	
-	    // great circle distance in radians
-	    double angle2 = 2 * Math.asin(Math.min(1, Math.sqrt(a)));
-	
-	    // convert back to degrees
-	    angle2 = Math.toDegrees(angle2);
-	
-	    // each degree on a great circle of Earth is 60 nautical miles
-	    double distNautMiles = 60 * angle2;
-	    
-	    double distMeters = distNautMiles * 1852;
-	    return distMeters;
-	}
-	
-	static double greatCircle( Coordinate p1, Coordinate p2 ){
-		return greatCircle( p1.x, p1.y, p2.x, p2.y );
-	}
+
 	
 }
