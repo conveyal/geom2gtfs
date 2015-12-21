b07519c9f0789a3047ed1ca62aefc021b5fe334e
Matthew Wigginton Conway <matt@indicatrix.org>
2015-12-21 13:06:20 -0500

    Fix one more issue with static state variables.

--- src/main/java/com/conveyal/geom2gtfs/ProtoRouteStop.java
+++ src/main/java/com/conveyal/geom2gtfs/ProtoRouteStop.java
@@ -5,8 +5,11 @@ import org.onebusaway.gtfs.model.Stop;
 
 import com.vividsolutions.jts.geom.Coordinate;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class ProtoRouteStop {
-        private static int createdStopId = 0;
+        // TODO: this is kind of hacky, we should really be using a separate ID space for each feed.
+        private AtomicInteger createdStopId = new AtomicInteger();
     
         public ProtoRouteStop (Coordinate coord, double dist) {
             this.coord = coord;
@@ -14,8 +17,9 @@ public class ProtoRouteStop {
             this.stop = new Stop();
             stop.setLat(coord.y);
             stop.setLon(coord.x);
-            stop.setName("Stop " + createdStopId++);
-            stop.setId(new AgencyAndId(Main.DEFAULT_AGENCY_ID, "created_stop_" + createdStopId));
+            int stopId = createdStopId.incrementAndGet();
+            stop.setName("Stop " + stopId);
+            stop.setId(new AgencyAndId(Main.DEFAULT_AGENCY_ID, "created_stop_" + stopId));
         }
         
         public ProtoRouteStop (Stop stop, double dist) {
